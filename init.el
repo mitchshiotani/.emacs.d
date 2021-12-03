95771f793db494e67df325aff54df4048a186eb6
mitch <mitch.shiotani1@gmail.com>
2021-12-04 01:56:31 +0900

    added some more stuff

--- init.el
+++ init.el
@@ -25,6 +25,8 @@
 
 ;; Packages ;;
 
+;; org mode ;;
+
 (use-package org
   :ensure t)
 (require 'org-tempo)
@@ -46,25 +48,30 @@
 	    ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
 	    ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
 	    (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-	;;(base-font-color     (face-foreground 'default nil 'default))
-	;;(headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-	(headline           `(:inherit default :weight bold)))
+	;; (base-font-color     (face-foreground 'default nil 'default))
+	;; (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+	(headline           `(:inherit default :weight bold :foreground "pink")))
 (custom-theme-set-faces
     'user
     `(org-level-8 ((t (,@headline ,@variable-tuple))))
     `(org-level-7 ((t (,@headline ,@variable-tuple))))
     `(org-level-6 ((t (,@headline ,@variable-tuple))))
     `(org-level-5 ((t (,@headline ,@variable-tuple))))
-    `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-    `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-    `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-    `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+    `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1 :foreground "light yellow"))))
+    `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25 :foreground "light green"))))
+    `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5 :foreground "light blue"))))
+    `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75 :foreground "red"))))
     `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
 
 (use-package evil
   :ensure t
   :config
   (evil-mode 1))
+
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
 
 ;; (use-package helm
 ;;   :ensure t)
@@ -83,6 +90,11 @@
 (use-package org-journal
   :ensure t)
 
+(use-package projectile
+  :ensure t)
+
+(global-set-key (kbd "C-c C-;") 'projectile-commander)
+
 (use-package plantuml-mode
   :ensure t)
 
@@ -96,10 +108,12 @@
   :config
   (auto-complete-mode 1))
 
+(auto-complete-mode 1)
+
 (use-package neotree
   :ensure t)
 ;; (global-set-key [f8] 'neotree-toggle)
-(global-set-key (kbd "C-c a") 'neotree-toggle)
+(global-set-key (kbd "C-c t") 'neotree-toggle)
 (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
 (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
 (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
@@ -144,11 +158,16 @@
 (global-set-key (kbd "C-c l") 'display-line-numbers-mode)
 (setq display-line-numbers-type 'relative)
 
+;; pomodoro
+(setq org-clock-sound "~/Music/epic.wav")
+
 ;; Theme ;;
 
-(use-package zenburn-theme
-  :ensure t)
+;; (use-package zenburn-theme
+;; :ensure t)
 ;; (load-theme 'zenburn t)
+
+(load-theme 'deeper-blue t)
 (load-theme 'deeper-blue t)
 (set-face-attribute 'default nil :height 150)
 
@@ -186,7 +205,26 @@
 
 ;; keybinding for org-capture, should be C-c C-c by default but wasn't working for some reason
 (global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
 (setq org-src-preserve-indentation t)
+
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
+
+; one escape to quit
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;visible bell (https://www.emacswiki.org/emacs/AlarmBell)
+(setq visible-bell nil
+      ring-bell-function 'double-flash-mode-line)
+(defun double-flash-mode-line ()
+  (let ((flash-sec (/ 1.0 20)))
+    (invert-face 'mode-line)
+    (run-with-timer flash-sec nil #'invert-face 'mode-line)
+    (run-with-timer (* 2 flash-sec) nil #'invert-face 'mode-line)
+    (run-with-timer (* 3 flash-sec) nil #'invert-face 'mode-line)))
 
 ;; Etc ;;
 
@@ -231,6 +269,8 @@
      (320 . "#8CD0D3")
      (340 . "#94BFF3")
      (360 . "#DC8CC3")))
+ '(auto-save-file-name-transforms '((".*" "~/.emacs-backups/autosaves/\\1" t)))
+ '(backup-directory-alist '((".*" . "~/.emacs-backups/backups/")))
  '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -243,17 +283,33 @@
 (toggle-tool-bar-mode-from-frame 0)
 (toggle-scroll-bar -1)
 
-(set-face-attribute 'default nil :font "Noto Sans Mono-11" )
-(set-frame-font "Noto Sans Mono-11" nil t)
+;; (set-face-attribute 'default nil :font "Noto Sans Mono-11" )
+;; (set-frame-font "Noto Sans Mono-11" nil t)
 
 ;; (evil-ex-define-cmd "ls" "buffers")
 ;; (evil-ex-define-cmd "buffers" 'buffer-menu-toggle-files-only)
 
 ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs-backups/.
-(custom-set-variables
-  '(auto-save-file-name-transforms '((".*" "~/.emacs-backups/autosaves/\\1" t)))
-  '(backup-directory-alist '((".*" . "~/.emacs-backups/backups/"))))
+;; (custom-set-variables
+;;   '(auto-save-file-name-transforms '((".*" "~/.emacs-backups/autosaves/\\1" t)))
+;;   '(backup-directory-alist '((".*" . "~/.emacs-backups/backups/"))))
 
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs-backups/autosaves/" t)
 (make-directory "~/.emacs-backups/backups/" t)
+
+(setq org-src-fontify-natively t)
+
+;; fixing the org code blocks.
+;; https://stackoverflow.com/questions/44811679/orgmode-change-code-block-background-color
+(custom-set-faces
+ ;; '(org-block-begin-line
+ ;;   ((t (:underline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF"))))
+ '(org-block
+   ;;((t (:background "#EFF0F1"))))
+   ((t (:background "#67595E"))))
+ ;; '(org-block-end-line
+ ;;    ((t (:overline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF"))))
+  )
+
+(blink-cursor-mode 0)
